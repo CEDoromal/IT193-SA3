6fffd70f6f76b0796c542078def0f85d54742d21
Katharsis009 <asphyxiaab@gmail.com>
2023-01-30 13:26:18 +0800

    HomeLogic1

--- app/src/main/java/group6/sa3/Home.java
+++ app/src/main/java/group6/sa3/Home.java
@@ -1,6 +1,10 @@
 package group6.sa3;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +14,27 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        final Button aboutButton = (Button) findViewById(R.id.abtBttn);
+        final Button adoptNow = (Button) findViewById(R.id.adoptbttn);
+        final Button outButton = (Button) findViewById(R.id.logoutButton);
+
+        final SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+
+        aboutButton.setOnClickListener(view -> {
+            startActivity(new Intent(getApplicationContext(), AboutUs.class));
+        });
+
+        adoptNow.setOnClickListener(view -> {
+            startActivity(new Intent(getApplicationContext(), DogList.class));
+        });
+
+        outButton.setOnClickListener(view -> {
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.remove("activeAccount");
+            finish();
+        });
+
     }
+
 }
