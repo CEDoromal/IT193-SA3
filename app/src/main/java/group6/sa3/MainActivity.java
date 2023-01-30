74d64166515d783985cdea0846a90fd4eeacba5f
CEDoromal <30488557+CEDoromal@users.noreply.github.com>
2023-01-30 12:20:08 +0800

    Update MainActivity

--- app/src/main/java/group6/sa3/MainActivity.java
+++ app/src/main/java/group6/sa3/MainActivity.java
@@ -3,9 +3,23 @@ package group6.sa3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+
+import group6.sa3.model.Account;
+import group6.sa3.retrofit.AccountApi;
+import group6.sa3.retrofit.RetrofitService;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,12 +28,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button loginBtn = (Button) findViewById(R.id.btnloginview);
-        loginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, Login.class));
-            }
+        final EditText personName = (EditText) findViewById(R.id.editTextTextPersonName);
+        final Button loginBtn = (Button) findViewById(R.id.btnlogin);
+
+        final SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        final RetrofitService retrofitService = new RetrofitService();
+        final AccountApi accountApi = retrofitService.getRetrofit().create(AccountApi.class);
+
+        loginBtn.setOnClickListener(view -> {
+            accountApi.getAccount(personName.getText().toString())
+                    .enqueue(new Callback<Account>() {
+                        @Override
+                        public void onResponse(Call<Account> call, Response<Account> response) {
+                            if(response.isSuccessful()) {
+                                Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                                SharedPreferences.Editor editor = sharedPref.edit();
+                                Gson gson = new Gson();
+                                editor.putString("activeAccount", gson.toJson(response.body()));
+                                editor.commit();
+                                startActivity(new Intent(getApplicationContext(), DogList.class));
+                            } else {
+                                Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<Account> call, Throwable t) {
+                            Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_LONG).show();
+                        }
+                    });
         });
     }
 }
